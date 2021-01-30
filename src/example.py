e74420239c4870fdfcfa4dab1846a08e6d33524b
desenk <desen.kirli@ed.ac.uk>
2021-01-30 11:55:18 +0000

    restructured the folders as src, lib, output, example and apikey

---
+++ src/example.py
@@ -0,0 +1,22 @@
+import BMRS_helper as bmrs
+
+
+df = bmrs.extract_data2('FORDAYDEM', '2020-01-27', '2020-01-28')
+print(df.head())
+
+# df1 = bmrs.generation('2020-01-27', '2020-01-28')
+# print(df1.head())
+
+# df1 = bmrs.loss_of_load('2020-01-27', '2020-01-28')
+
+# print(df1.head())
+# print(df1.tail())
+
+
+# df1 = bmrs.frequency('2020-01-27', '2020-01-28')
+# print(df1.head())
+# print(df1.describe())
+
+df1 = bmrs.initial_demand_transmission('2020-01-27', '2020-01-28')
+print(df1.head())
+print(df1.describe())
